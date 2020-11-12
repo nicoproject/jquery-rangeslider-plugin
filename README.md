84feed65d382ae82f76b91f9bf600963a10b83dd
Nico4000 <nicoproject@ya.ru>
2020-11-12 19:58:00 +0300

    Environment setup completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# jQuery slider plugin
+
+jQuery slider plugin
---
+++ src/js/demo.js
@@ -0,0 +1,3 @@
+const demo = () => 'Webpack Boilerplate v5.0.1 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+
+console.log(demo());
